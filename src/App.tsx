bb3e8b31f2a77dff8aa9d227bf953b49e5e92363
tauhidul0821 <tuhin2093@gmail.com>
2025-01-07 17:15:58 +0600

    react router working fine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Route,  Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
   return (
     <>
-      Hello, World
+        <Router>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
     </>
   )
 }
